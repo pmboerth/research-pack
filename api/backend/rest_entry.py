1ceffca8405cef4e1c4f7380920e1bde1ba3e26e
pmboerth <pmboerth@gmail.com>
2024-12-01 14:38:10 -0500

    register opportunity routes and remove unused routes

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -1,8 +1,7 @@
 from flask import Flask
 
 from backend.db_connection import db
-from backend.customers.customer_routes import customers
-from backend.products.products_routes import products
+from backend.opportunities.opportunities_routes import opportunities
 from backend.simple.simple_routes import simple_routes
 import os
 from dotenv import load_dotenv
@@ -40,8 +39,7 @@ def create_app():
     # and give a url prefix to each
     app.logger.info('current_app(): registering blueprints with Flask app object.')   
     app.register_blueprint(simple_routes)
-    app.register_blueprint(customers,   url_prefix='/c')
-    app.register_blueprint(products,    url_prefix='/p')
+    app.register_blueprint(opportunities,   url_prefix='/o')
 
     # Don't forget to return the app object
     return app
