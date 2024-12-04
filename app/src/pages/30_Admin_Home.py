ae68e02d03057e173af0a120d8a43596dd1cef34
thokeefe <okeefe.th@northeastern.edu>
2024-12-04 13:59:04 -0500

    admin pages

--- app/src/pages/30_Admin_Home.py
+++ app/src/pages/30_Admin_Home.py
@@ -19,27 +19,27 @@ if st.button('See all Research Opportunities',
              use_container_width=True):
   st.switch_page('pages/01_All_Opportunities.py')
 
-if st.button('Delete Research Opportunities', 
+if st.button('Delete uncompliant Research Opportunities', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/02_See_Posts.py')
+  st.switch_page('pages/32_Delete_Opportunity.py')
 
 if st.button('See all Students', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/02_See_Posts.py')
+  st.switch_page('pages/33_All_Students.py')
 
 if st.button('Remove ineligible Students', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/02_See_Posts.py')
+  st.switch_page('pages/34_Remove_Student.py')
 
 if st.button('Generate reports on student engagement', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/02_See_Posts.py')
+  st.switch_page('pages/35_Generate_Report.py')
 
 if st.button('Update Student profiles', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/02_See_Posts.py')
+  st.switch_page('pages/36_Update_Student.py')
