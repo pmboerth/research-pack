443829af43ec31a776d74a114271d1335a12457c
Luca Vasiliu <LucaVasiliu@MacBook-Pro-5.local>
2024-12-05 01:14:32 -0500

    Updates to posted positions page

--- app/src/pages/22_Posted_Positions.py
+++ app/src/pages/22_Posted_Positions.py
@@ -17,7 +17,7 @@ response = requests.get(f"http://api:4000/o/opportunities/o{ownerID}")
 
 if response.status_code == 200:
     results = response.json()
-
+    
     if results:
         for position in results:
             research_title = position.get('Name', 'No Title')
@@ -36,11 +36,11 @@ if response.status_code == 200:
             st.markdown(f"""
                 <div style="border: 1px solid #3D4A59; padding: 20px; margin-bottom: 20px; border-radius: 8px; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2); background-color: #3D4A59;">
                     <h3 style="margin-bottom: 10px; color: #90AEAD;">{research_title}</h3>
-                    <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Group:</strong> Emily Chen</p>
-                    <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Student:</strong> {research_area}</p>
-                    <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Department:</strong> {department_name}</p>
+                    <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Faculty: </strong>Professor Emily Chen</p>
+                    <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Research Area: </strong> {research_area}</p>
+                    <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Department: </strong> {department_name}</p>
                     <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Required Skill:</strong> {skill_id}</p>
-                    <p style="margin-bottom: 12px; color: #D1D7DC;">{research_description}</p>
+                    <p style="margin-bottom: 12px; color: #D1D7DC;"><strong>Description:</strong> {research_description}</p>
                 </div>
             """, unsafe_allow_html=True)
     else:
