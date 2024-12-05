9b0ce1fd7322864d834d9539e563d7063b841d68
pmboerth <pmboerth@gmail.com>
2024-12-05 00:23:55 -0500

    view professor research opportunities

--- app/src/pages/22_Posted_Positions.py
+++ app/src/pages/22_Posted_Positions.py
@@ -13,33 +13,33 @@ st.title('My Posted Research Positions')
 
 ownerID = st.session_state['professor_id']
 
-response = requests.get("http://api:4000/opportunities/o{ownerID}")
+response = requests.get(f"http://api:4000/o/opportunities/o{ownerID}")
 
 if response.status_code == 200:
     results = response.json()
 
     if results:
         for position in results:
-            research_title = position.get['research_title', 'No Title']
-            professor_id = position.get['professor_id', 'No ID']
-            research_area = position.get['research_area', 'No Area']
-            research_description = position.get['research_description', 'No Description']
-            department_id = position.get['department_id', 'No Department']
-            skill_id = position.get['skill_id', 'No Skill']
+            research_title = position.get('Name', 'No Title')
+            research_area = position.get('ResearchArea', 'No Area')
+            research_description = position.get('Description', 'No Description')
+            department_id = position.get('DepartmentId', 'No Department')
+            skill_id = position.get('SkillId', 'No Skill')
+            
+            # Fetch department name based on department ID
+            result1 = requests.get(f'http://api:4000/d/departments/d{department_id}').json()
+            department_name = result1[0].get('Name', 'No Name')
+    
 
-            prof_name_result = requests.get(f'http://api:4000/professors/p{professor_id}').json()
-            professor_firstname = prof_name_result[0].get('FirstName', 'No First Name')
-            professor_lastname = prof_name_result[0].get('LastName', 'No Last Name')
-            professor_name = f'Professor {professor_firstname} {professor_lastname}'
 
             # Display each post using styled HTML
             st.markdown(f"""
                 <div style="border: 1px solid #3D4A59; padding: 20px; margin-bottom: 20px; border-radius: 8px; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2); background-color: #3D4A59;">
                     <h3 style="margin-bottom: 10px; color: #90AEAD;">{research_title}</h3>
-                    <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Group:</strong> {professor_name}</p>
+                    <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Group:</strong> Emily Chen</p>
                     <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Student:</strong> {research_area}</p>
-                    <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Created At:</strong> {department_id}</p>
-                    <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Created At:</strong> {skill_id}</p>
+                    <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Department:</strong> {department_name}</p>
+                    <p style="margin-bottom: 8px; color: #D1D7DC;"><strong>Required Skill:</strong> {skill_id}</p>
                     <p style="margin-bottom: 12px; color: #D1D7DC;">{research_description}</p>
                 </div>
             """, unsafe_allow_html=True)
