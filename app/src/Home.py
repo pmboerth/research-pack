4e457f626798405d398c0ad02685a3326dc3adc7
tiashag <117884255+tiashag@users.noreply.github.com>
2024-12-04 22:39:40 -0500

    updated comments

--- app/src/Home.py
+++ app/src/Home.py
@@ -7,38 +7,34 @@ import logging
 logging.basicConfig(format='%(filename)s:%(lineno)s:%(levelname)s -- %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# import the main streamlit library as well
-# as SideBarLinks function from src/modules folder
+# import the main streamlit library as well as SideBarLinks function from src/modules folder
 import streamlit as st
 from modules.nav import SideBarLinks
 
-# streamlit supports reguarl and wide layout (how the controls
-# are organized/displayed on the screen).
+# streamlit supports regular and wide layout (how the controls are organized/displayed on the screen).
 st.set_page_config(layout = 'wide')
 
-# If a user is at this page, we assume they are not 
-# authenticated.  So we change the 'authenticated' value
+# If a user is at this page, we assume they are not authenticated.  So we change the 'authenticated' value
 # in the streamlit session_state to false. 
 st.session_state['authenticated'] = False
 
-# Use the SideBarLinks function from src/modules/nav.py to control
-# the links displayed on the left-side panel. 
+# Use the SideBarLinks function from src/modules/nav.py to control the links displayed on the left-side panel. 
 # IMPORTANT: ensure src/.streamlit/config.toml sets
 # showSidebarNavigation = false in the [client] section
 SideBarLinks(show_home=True)
 
 # ***************************************************
-#    The major content of this page
+#        The major content of this page
 # ***************************************************
 
-# set the title of the page and provide a simple prompt. 
+# set the title of the page and provide a simple prompt
 logger.info("Loading the Home page of the app")
 st.title('ResearchPack')
 st.write('\n\n')
 st.write('### Hi! As which user would you like to log in?')
 
 # For each of the user personas for which we are implementing
-# functionality, we put a button on the screen that the user 
+# functionality we put a button on the screen that the user 
 # can click to MIMIC logging in as that mock user. 
 
 if st.button("Act as Alex Bellingham, an Undergraduate Student", 
